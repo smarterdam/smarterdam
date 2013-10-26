e879157a14afbc22cc299a26d0b117df74d54033
Yury <y.timofeyev@gmail.com>
2013-10-26 17:17:06 +0400

    Test project configuration

---
+++ Smarterdam.Tests/NeuralNetworkPipelineTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace Smarterdam.Tests
+{
+    [TestFixture]
+    public class NeuralNetworkPipelineTest
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.AreEqual(1, 1);
+        }
+    }
+}
